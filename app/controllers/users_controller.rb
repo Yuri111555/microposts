91e34752e4c796696211b03b7b45c5cb1f08bc71
yuri.takemoto <higumaotoshi@hotmail.com>
2019-02-15 15:24:28 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  
-  before_action :require_user_logged_in, only: [:index, show]
-  
-  
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
@@ -33,7 +32,7 @@ class UsersController < ApplicationController
   
 private
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  
-end
+ def user_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+ end
+  end
